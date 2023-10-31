0d2748d073a74dad00bfe1cbe7a71ce75f94f3a3
Pilmo <pilmokim99@gmail.com>
2023-10-31 11:44:26 +0900

    detach  server

--- server/routes/JSON_format.py
+++
@@ -1,7 +0,0 @@
-from fastapi.responses import JSONResponse
-def JSON_format(message: str, data = {}):
-    response = {
-        "message": message,
-        "data": data
-    }
-    return response
